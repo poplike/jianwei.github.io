cc2eefcc623091f9f54b9c2dee3d4932072465d1
jianwei03 <chenjw@51mydao.com>
2017-09-19 09:25:44 +0800

    add dev

---
+++ src/views/DesignPatterns/prototype.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>原型</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/home.vue
@@ -0,0 +1,15 @@
+<template> 
+  <div>
+    <h2>改变</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
